a4b6add71547936d4b6daebebd7df6488e1ca0ae
hina <hina@hinaloe.net>
2017-12-27 05:36:37 +0900

    fix Attachment

---
+++ TootStreamView/Entities/Attachment.cs
@@ -0,0 +1,46 @@
+﻿// original by https://github.com/amay077/MastoConsoleNetStandard
+// MIT License Copyright (c) 2017 amay077
+
+using Newtonsoft.Json;
+
+namespace TootStreamView.Entities
+{
+    public class Attachment
+    {
+        /// <summary>
+        /// ID of the attachment
+        /// </summary>
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// One of: "image", "video", "gifv"
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// URL of the locally hosted version of the image
+        /// </summary>
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// For remote images, the remote URL of the original image
+        /// </summary>
+        [JsonProperty("remote_url")]
+        public string RemoteUrl { get; set; }
+
+        /// <summary>
+        /// URL of the preview image
+        /// </summary>
+        [JsonProperty("preview_url")]
+        public string PreviewUrl { get; set; }
+
+        /// <summary>
+        /// Shorter URL for the image, for insertion into text (only present on local images)
+        /// </summary>
+        [JsonProperty("text_url")]
+        public string TextUrl { get; set; }
+    }
+}
--- TootStreamView/Entities/Status.cs
+++ TootStreamView/Entities/Status.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Net.Mail;
 using Newtonsoft.Json;
 
 namespace TootStreamView.Entities
